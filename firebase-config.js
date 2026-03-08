cbabdb75fe984dd37852c5674348677db5b65896
Roger Chui <65433947+Leonel-calle-dev@users.noreply.github.com>
2026-03-07 22:55:25 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,22 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD2wpaEAQzrcfCoSnjBSavOTTJ1xnv2k2M",
-    authDomain: "campus-palmas.firebaseapp.com",
-    databaseURL: "https://campus-palmas-default-rtdb.firebaseio.com",
-    projectId: "campus-palmas",
-    storageBucket: "campus-palmas.firebasestorage.app",
-    messagingSenderId: "1042297524028",
-    appId: "1:1042297524028:web:0af2592e782ab4ee871839",
-    measurementId: "G-KZFTX9BXJZ"
-  };
+  apiKey: "AIzaSyD2wpaEAQzrcfCoSnjBSavOTTJ1xnv2k2M",
+  authDomain: "campus-palmas.firebaseapp.com",
+  databaseURL: "https://campus-palmas-default-rtdb.firebaseio.com",
+  projectId: "campus-palmas",
+  storageBucket: "campus-palmas.firebasestorage.app",
+  messagingSenderId: "1042297524028",
+  appId: "1:1042297524028:web:100618ff445a4134871839",
+  measurementId: "G-H192H191PX"
+};
 
-const app  = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db   = getFirestore(app);
-
-export { app, auth, db };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
