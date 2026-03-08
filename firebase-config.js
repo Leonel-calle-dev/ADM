d8687607c041b17723258e248915a1664611dec2
Roger Chui <65433947+Leonel-calle-dev@users.noreply.github.com>
2026-03-07 23:03:30 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD2wpaEAQzrcfCoSnjBSavOTTJ1xnv2k2M",
-  authDomain: "campus-palmas.firebaseapp.com",
-  databaseURL: "https://campus-palmas-default-rtdb.firebaseio.com",
-  projectId: "campus-palmas",
-  storageBucket: "campus-palmas.firebasestorage.app",
-  messagingSenderId: "1042297524028",
-  appId: "1:1042297524028:web:100618ff445a4134871839",
-  measurementId: "G-H192H191PX"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD2wpaEAQzrcfCoSnjBSavOTTJ1xnv2k2M",
+    authDomain: "campus-palmas.firebaseapp.com",
+    databaseURL: "https://campus-palmas-default-rtdb.firebaseio.com",
+    projectId: "campus-palmas",
+    storageBucket: "campus-palmas.firebasestorage.app",
+    messagingSenderId: "1042297524028",
+    appId: "1:1042297524028:web:100618ff445a4134871839",
+    measurementId: "G-H192H191PX"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
